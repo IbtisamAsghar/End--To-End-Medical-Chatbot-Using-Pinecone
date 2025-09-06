794f19688b8e84cc5aa63a19319257a4d3e3d876
Ibtisam <ibtisamasghar71@gmail.com>
2025-09-06 19:26:52 +0500

    Setup is completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name="Medical_ChatBot",
+    version="0.1.0",
+    author="Ibtisam Asghar",
+    author_email="ibtisamasghar71@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
